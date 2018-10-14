d912590679ddf87e7fa8c5e6d4bb646a70e660c4
Ji S <j2yshin@gmail.com>
2018-10-14 23:31:10 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -33,7 +33,7 @@ end
 def while_iterator(number_of_times)
   counter = 0
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  while counter < number_of_times + 1
+  while counter != number_of_times
     puts phrase
     counter += 1
   end
